2f1f7b383092d6e166d903c158c6e1f03aaa02eb
poclus2 <haressarl@gmail.com>
2025-12-14 22:29:37 +0100

    Fix: Clean up ENS fix by using checksum addresses for contracts

--- src/services/blockchain.service.js
+++ src/services/blockchain.service.js
@@ -10,17 +10,11 @@ class BlockchainService {
     const network = new ethers.Network("custom-celo", chainId);
     network.ensAddress = null; // Désactivation explicite de l'ENS
 
-    // Astuce Ethers v6: passer 'undefined' en 2eme arg et le network dans les options
-    this.provider = new ethers.JsonRpcProvider(config.celoRpcUrl, undefined, { staticNetwork: network });
+    this.provider = new ethers.JsonRpcProvider(config.celoRpcUrl, network, { staticNetwork: network });
 
-    // MONKEY PATCH: Surcharge radicale pour interdire toute résolution ENS
-    this.provider.resolveName = async () => null;
-    this.provider.lookupAddress = async () => null;
-    this.provider.getEnsAddress = async () => null;
-
-    // Initialisation du contrat Token
+    // Initialisation du contrat Token avec adresse checksummed
     this.tokenContract = new ethers.Contract(
-      config.tokenContractAddress,
+      ethers.getAddress(config.tokenContractAddress),
       TOKEN_ABI,
       this.provider
     );
